d6b81050bc269e4ca9b6dc83c32a0dc21e6b3b6e
Carlos Silva <carlos@bindtuning.com>
2022-04-07 23:35:05 +0100

    Print http.Request

--- main.go
+++ main.go
@@ -97,6 +97,7 @@ func main() {
 			bro_name, _ := ua.Browser()
 			allowedUA := strings.Contains(r.UserAgent(), "ISV|BindTuning|BindTuning Provisioning/1.0") || strings.Contains(r.UserAgent(), "NONISV|SharePointPnP")
 			if ua.Bot() || bro_name == "curl" || !allowedUA {
+				log.Println(r)
 				return r, goproxy.NewResponse(r, goproxy.ContentTypeText, http.StatusForbidden, "Don't waste your time!")
 			}
 
