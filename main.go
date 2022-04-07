2ddacd2c40fc76abb3b15f08bdf1be5c55b242bc
Carlos Silva <carlos@bindtuning.com>
2022-04-07 23:57:05 +0100

    Add r.RemoteAddr

--- main.go
+++ main.go
@@ -97,7 +97,7 @@ func main() {
 			bro_name, _ := ua.Browser()
 			allowedUA := strings.Contains(r.UserAgent(), "ISV|BindTuning|BindTuning Provisioning/1.0") || strings.Contains(r.UserAgent(), "NONISV|SharePointPnP")
 			if ua.Bot() || bro_name == "curl" || !allowedUA {
-				log.Println(r)
+				log.Println(r.RemoteAddr)
 				return r, goproxy.NewResponse(r, goproxy.ContentTypeText, http.StatusForbidden, "Don't waste your time!")
 			}
 
