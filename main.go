063e42894b8da933f9f34992933e54730f2a58eb
Carlos Silva <carlos@bindtuning.com>
2022-04-08 14:22:30 +0100

    Log Referrer

--- main.go
+++ main.go
@@ -98,7 +98,7 @@ func main() {
 			bro_name, _ := ua.Browser()
 			allowed_ua := strings.Contains(req_ua, "ISV|BindTuning") || strings.Contains(req_ua, "NONISV|SharePointPnP")
 			if ua.Bot() || bro_name == "curl" || !allowed_ua {
-				log.Println(r, ctx)
+				log.Println(r.Referrer)
 				return r, goproxy.NewResponse(r, goproxy.ContentTypeText, http.StatusForbidden, "Don't waste your time!")
 			}
 
